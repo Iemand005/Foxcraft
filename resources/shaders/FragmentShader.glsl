4fa827c53598163bd6ed3473dff8b8434682210a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-08 22:22:37 +0200

    okkk array we gooo

--- resources/shaders/FragmentShader.glsl
+++ resources/shaders/FragmentShader.glsl
@@ -3,27 +3,21 @@ out vec4 FragColor;
 
 in vec3 Normal;
 in vec3 FragPos;
-in vec2 TexCoord;
+in vec3 TexCoord;
 
-uniform sampler2D ourTexture;
-
-struct PointLight {
-    vec3 position;
-    vec3 color;
-    float intensity;
-    float radius;
-};
-
-uniform int lightCount;
-uniform PointLight pointLights[8];
+uniform sampler2DArray ourTexture; 
 
 void main()
 {
     vec3 n = normalize(Normal);
 
-    vec3 lighting = vec3(1);
-
+    vec3 lighting = vec3(1.0); 
 
     vec4 texSample = texture(ourTexture, TexCoord);
+    
+    if(texSample.a < 0.1) {
+        discard;
+    }
+
     FragColor = vec4(texSample.rgb * lighting, texSample.a);
 }
