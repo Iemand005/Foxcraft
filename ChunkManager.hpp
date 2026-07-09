6b788dc2f029a015e31599f6926b1eb0b5ca17fc
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-10 01:41:17 +0200

    ineed the depshere tho

--- ChunkManager.hpp
+++ ChunkManager.hpp
@@ -114,29 +114,8 @@ private:
     }
 
     void UploadAndInsert(std::shared_ptr<Chunk> chunk) {
-        // Safe here: this runs on main thread, GL context is current
-        // glGenVertexArrays(1, &chunk->vao);
-        // glGenBuffers(1, &chunk->vbo);
-        // glGenBuffers(1, &chunk->ebo);
 
-        // glBindVertexArray(chunk->vao);
-        // glBindBuffer(GL_ARRAY_BUFFER, chunk->vbo);
-        // glBufferData(GL_ARRAY_BUFFER, chunk->vertexData.size() * sizeof(Vertex),
-        //              chunk->vertexData.data(), GL_STATIC_DRAW);
-
-        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, chunk->ebo);
-        // glBufferData(GL_ELEMENT_ARRAY_BUFFER, chunk->indexData.size() * sizeof(uint32_t),
-        //              chunk->indexData.data(), GL_STATIC_DRAW);
-        // // ... vertex attrib pointers ...
-
-        // chunk->sceneNode = scene->AddChunkNode(chunk);
-        // chunk->state = ChunkState::InScene;
-
-        // // Free CPU-side copies now that they're on the GPU
-        // chunk->vertexData.clear();
-        // chunk->vertexData.shrink_to_fit();
-        // chunk->indexData.clear();
-        // chunk->indexData.shrink_to_fit();
+		// chunk->UploadToScene()
     }
 
     void RemoveFromScene(std::shared_ptr<Chunk> chunk) {
