bba4164efe6a5852fb9c21e15419d06225167b05
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-10 17:39:41 +0200

    bababiyou

---
+++ ChunkMesher.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+
+class ChunkMesher {
+public:
+    static Mesh BuildMesh(Chunk& chunk, ChunkManager& manager) {
+        Mesh mesh;
+        ChunkCoord coord = chunk.GetCoord();
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int z = 0; z < DEPTH; z++) {
+                    BlockType block = chunk.GetBlock(x, y, z);
+                    if (block == BlockType::Air) continue;
+
+                    int worldX = coord.x * WIDTH + x;
+                    int worldZ = coord.y * DEPTH + z;
+
+                    // for each of 6 directions, check neighbor via manager
+                    if (manager.GetBlock(worldX - 1, y, worldZ) == BlockType::Air)
+                        mesh.AddFace(Face::Left, x, y, z, block);
+                    // ...repeat for +x, -y, +y, -z, +z
+                }
+            }
+        }
+        return mesh;
+    }
+};
