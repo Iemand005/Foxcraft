c19e16e3cd41f3e5109af374f00d52b05fbd0f3f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-10 18:02:19 +0200

    Move implementation that depends

---
+++ Chunk.cpp
@@ -0,0 +1,38 @@
+#include "Chunk.hpp"
+
+void Chunk::UploadToScene(fe::PhysicsFactory* physicsEngine, fe::Scene* scene) {
+
+	if (state == ChunkState::ScheduledForRemoval || state == ChunkState::RemovalPending) {
+		std::cout << "Cancelled chunk upload (" << coord.x << ", " << coord.y << ") due to removal request." << std::endl;
+
+		return; // got cancelled after the caller's check but before we ran
+	}
+	std::cout << "Uploading chunk (" << coord.x << ", " << coord.y << "): " << "Vertices: " << mesh.vertices.size() << " Indices: " << mesh.indices.size() << std::endl;
+	
+	// GPU coies
+	mesh.CopyToGPU();
+	mesh.loadTextureArray(ChunkMesher::BlockTextures(), fe::TextureScaling::Nearest);
+
+	std::vector<glm::vec3> colliderVertices;
+	colliderVertices.reserve(mesh.vertices.size());
+	for (const auto& vertex : mesh.vertices)
+		colliderVertices.push_back(vertex.position);
+
+	std::vector<uint32_t> colliderIndices(mesh.indices.begin(), mesh.indices.end());
+	
+	
+	sceneObject = std::make_shared<fe::Object>(std::move(mesh));
+	sceneObject->name = "Chunk";
+	sceneObject->state.position = GetWorldPosition();
+	
+	auto physobj = physicsEngine->CreateObject(colliderVertices, colliderIndices);
+	if (physobj) {
+		physobj->SetPosition(sceneObject->state.position);
+	}
+	mesh.SetPhysicsObject(std::move(physobj));
+	
+	mesh.FreeCpuData();
+
+	scene->AddObject(sceneObject);
+	state = ChunkState::InScene;
+}
