0cdbc7a5a668cd12fc2a5444e72b08a859082f1d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-23 13:12:37 +0200

    tweakjump strenthg

--- Foxcraft.hpp
+++ Foxcraft.hpp
@@ -224,7 +224,7 @@ public:
 		this->scene->AddObject(player);
 		this->player->state.position = glm::vec3(0.0f, 35.0f, 5.0f);
 		this->player->gravityEnabled = true;
-		this->player->jumpSpeed = 8.5f;
+		this->player->jumpSpeed = 7.07f; // 1.25 blocks at gravity -20
 		this->player->moveSpeed = walkSpeed;
 		RebuildPlayerPhysicsBody();
 		UpdateLoadedChunks();
